1f5397b82246d178dcafffa69532ad61859d6512
Tony.Wu <uq44961879@gmail.com>
2023-06-21 22:54:08 +1000

    <dev>: test for updating on AWS Amplify.

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Dashboard
+    </div>
+    <el-row>
+                <el-button type="primary">Primary</el-button>
+                <el-button type="success" :icon="Check" circle />
+                <el-button type="info" :icon="Message" circle />
+                <el-button type="warning" :icon="Star" circle />
+                <el-button type="danger" :icon="Delete" circle />
+            </el-row>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
