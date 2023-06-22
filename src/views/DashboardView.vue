fe203a05dd45a78a13f5fda08cb9aebb7577b70d
Tony.Wu <uq44961879@gmail.com>
2023-06-22 14:48:55 +1000

    <dev>: test for updating on AWS Amplify.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -29,32 +29,63 @@
                 </el-table-column>
                 <el-table-column prop="amount" label="Amount" sortable width="160" />
             </el-table>
-            <!-- <el-row>
+            <el-row>
                 <el-button type="primary">Primary</el-button>
                 <el-button type="success" :icon="Check" circle />
                 <el-button type="info" :icon="Message" circle />
                 <el-button type="warning" :icon="Star" circle />
                 <el-button type="danger" :icon="Delete" circle />
-            </el-row> -->
+            </el-row>
         </el-tab-pane>
-        <el-tab-pane label="Dashboard"> Dashboards </el-tab-pane>
+        <el-tab-pane label="Dashboard" > 
+            <div class="content-box">
+                <p> Dashboards </p> 
+                <el-row>
+                    <el-card shadow="hover" :style="{width:'350px'}" :body-style="{ padding: '0px' }">
+                        <div class="grid-content grid-con-1">
+                            <Goods class="grid-con-icon"/>
+                            <div class="grid-cont-right">
+                                <div class="grid-num">$ 536.23</div>
+                                <div> Spending </div>
+                            </div>
+                        </div>
+                    </el-card>
+                
+                    <el-card shadow="hover" :style="{width:'350px'}" :body-style="{ padding: '0px' }">
+                        <div class="grid-content grid-con-2">
+                            <Money class="grid-con-icon"/>
+                            <div class="grid-cont-right">
+                                <div class="grid-num">$ 1334.26</div>
+                                <div> Income </div>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-row>
+
+
+            </div>
+        </el-tab-pane>
         <el-tab-pane label="Management"> Management </el-tab-pane>
     </el-tabs>
 </template>
 
 <script lang="ts" setup>
-    // import {
-    //     Check,
-    //     Delete,
-    //     Edit,
-    //     Message,
-    //     Search,
-    //     Star,
-    //     } from '@element-plus/icons-vue'
-    import { ref } from 'vue'
-    import type { TableColumnCtx, TableInstance } from 'element-plus'
+    import {
+        Check,
+        Delete,
+        Edit,
+        Message,
+        Search,
+        Star,
+        Goods,
+        Money
+        } from '@element-plus/icons-vue'
+    import { onMounted, ref } from 'vue'
 
-    
+    const value1 = ref('')
+
+    const size = ref<'default' | 'large' | 'small'>('default')
+
     const tabPosition = ref('left')
 
     interface Transaction {
@@ -63,9 +94,8 @@
         amount: Number
     }
 
-    const tableRef = ref<TableInstance>()
 
-    const filterClass = (value: String, row: Transaction, column: TableColumnCtx<Transaction>) => {
+    const filterClass = (value: String, row: Transaction) => {
         return row.class.includes(value)
     }
 
@@ -89,7 +119,55 @@
 </script>
 
 <style>
+.content-box {
+    background-color: #f0f0f0;
+    padding: 10px;
+}
 .el-row {
   margin: 20px;
 }
+
+.grid-content {
+	display: flex;
+	align-items: center;
+	height: 100px;
+}
+
+.grid-cont-right {
+	flex: 1;
+	text-align: center;
+	font-size: 14px;
+	color: #999;
+}
+
+.grid-num {
+    font-size: 30px;
+	font-weight: bold;
+}
+
+.grid-con-icon {
+    width: 100px;
+	height: 100px;
+    padding: 30px;
+	text-align: center;
+	line-height: 100px;
+	color: #fff;
+}
+
+.grid-con-1 .grid-con-icon {
+	background: rgb(240, 210, 113);
+}
+
+.grid-con-2 .grid-con-icon {
+	background: rgb(100, 213, 114);
+}
+
+.grid-con-1 .grid-num {
+	color: rgb(240, 210, 113);
+}
+
+.grid-con-2 .grid-num {
+	color: rgb(100, 213, 114);
+}
+
 </style>
